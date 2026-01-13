af2449af7cc27e5743d3b66bb13db5a2845bafbc
jojo2525 <walla2525@ewhain.net>
2026-01-13 21:45:04 +0900

    메이븐으로 변경 전 커밋

--- src/main/java/com/dramalog/repository/ReviewRepository.java
+++ src/main/java/com/dramalog/repository/ReviewRepository.java
@@ -1,5 +1,14 @@
 package com.dramalog.repository;
 
-public class ReviewRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.dramalog.model.Review;
 
+public interface ReviewRepository extends JpaRepository<Review, Integer>{
+	
+	// List<Review> findByEpisode(Integer dramaId, Integer episodeSelected);
+	// List<Review> findByDrama(Integer dramaId);
+	List<Review> findByDramaIdAndEpisodeSelected(Integer dramaId, Integer episodeSelected);
+	List<Review> findByDramaId(Integer dramaId);
+	List<Review> findByUserId(Integer userId);
 }
